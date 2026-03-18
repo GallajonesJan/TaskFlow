aabb0fc5128f232f340335137ac93e5ec317c63c
Jan Lauren Gallajones <gallajonesjanlauren@gmail.com>
2026-03-18 23:15:00 +0800

    vercl

--- src/services/api.js
+++ src/services/api.js
@@ -9,7 +9,7 @@
  *   const data = await api.post('/tasks', { title: '...' });
  */
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 
 let _userEmail = '';
 
